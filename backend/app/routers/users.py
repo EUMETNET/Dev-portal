93a1f24bebc8d0e48478ead2d47e0c2b07724be4
aaltonja <jani.aaltonen@cgi.com>
2024-04-23 09:59:22 +0300

    add delete /users/<id>/apikey route for admins

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -8,9 +8,10 @@ from httpx import AsyncClient
 from app.config import settings, logger
 from app.dependencies.jwt_token import validate_admin_role, AccessToken
 from app.dependencies.http_client import get_http_client
-from app.services import users
+from app.services import users, apikey
 from app.models.responses import DeleteAPIKey
 from app.exceptions import APISIXError, VaultError, KeycloakError
+from app.utils.uuid import remove_dashes
 
 router = APIRouter()
 
@@ -27,6 +28,7 @@ async def delete_user(
     Delete a user from Keycloak and user's API key from Vault and APISIX(es).
 
     Args:
+        user_uuid (str): The UUID of the user whose API key is to be deleted.
         token (AccessToken): The access token of the user to be deleted.
         client (AsyncClient): The HTTP client to use for making requests.
 
@@ -45,6 +47,48 @@ async def delete_user(
         await users.delete_user(client, user_uuid)
 
     except (VaultError, APISIXError, KeycloakError) as e:
+        raise HTTPException(status_code=HTTPStatus.SERVICE_UNAVAILABLE, detail=str(e)) from e
+
+    return DeleteAPIKey()
+
+
+@router.delete("/users/{user_uuid}/apikey", response_model=DeleteAPIKey)
+async def delete_user_apikey(
+    user_uuid: str,
+    token: AccessToken = Depends(validate_admin_role),
+    client: AsyncClient = Depends(get_http_client),
+) -> DeleteAPIKey:
+    """
+    Delete a user's API key from Vault and APISIX(es).
+
+    Args:
+        user_uuid (str): The UUID of the user whose API key is to be deleted.
+        token (AccessToken): The access token of the user whose API key is to be deleted.
+        client (AsyncClient): The HTTP client to use for making requests.
+
+    Returns:
+        JSONResponse: A response indicating whether the deletion was successful.
+
+    Raises:
+        HTTPException: If there is an error getting user info from APISIX or Vault,
+                       or if there is an error deleting the user from Vault or APISIX.
+    """
+    admin_uuid = token.sub
+    user_uuid_no_dashes = remove_dashes(user_uuid)
+
+    logger.info("Admin '%s' requested deletion of API key for user '%s'", admin_uuid, user_uuid)
+
+    try:
+        vault_user, apisix_users = await apikey.get_user_from_vault_and_apisixes(
+            client, user_uuid_no_dashes
+        )
+        if vault_user or any(apisix_users):
+            logger.debug("User '%s' found in Vault and/or APISIX --> Deleting user", user_uuid)
+            await apikey.delete_user_from_vault_and_apisixes(
+                client, user_uuid_no_dashes, vault_user, apisix_users
+            )
+
+    except (VaultError, APISIXError) as e:
         raise HTTPException(status_code=HTTPStatus.SERVICE_UNAVAILABLE, detail=str(e)) from e
 
     return DeleteAPIKey()
