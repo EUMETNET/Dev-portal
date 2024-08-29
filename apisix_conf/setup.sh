45a47991505a052591708af122a67109fdc8c6d5
aaltonja <jani.aaltonen@cgi.com>
2024-08-29 10:46:27 +0300

    fix apisix related service not ready errors, print better error, exit with error

--- apisix_conf/setup.sh
+++ apisix_conf/setup.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-APISIX_ADMIN_API_PORTS=9180,9280
+APISIX_ADMIN_API_PORTS=9180,9289
+
+# Function to check the response status
+check_response() {
+    local response="$1"
+    local port="$2"
+    local status_code=$(echo "$response" | grep HTTP | awk '{print $2}')
+    if [ -z "$status_code" ]; then
+        echo "Request failed for admin API on port $port: No response from server"
+        exit 1
+    elif [ "$status_code" -ne 200 ] && [ "$status_code" -ne 201 ]; then
+        echo "Request failed for admin API on port $port with status code $status_code"
+        exit 1
+    fi
+}
 
 # Assuming you have two instances running on ports 9180 and 9280
+echo "Configuring Vault secrets engine and consumer groups for APISIX instances"
 for port in ${APISIX_ADMIN_API_PORTS//,/ }
 do
-    echo "Configuring Vault secrets engine for APISIX instance on port $port"
     response=$(curl -i -X PUT \
         -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' \
         -d '{
@@ -14,12 +28,9 @@ do
             "token": "'"$VAULT_TOKEN"'"
         }' \
         "http://localhost:$port/apisix/admin/secrets/vault/dev" 2>&1)
-    
-    #echo "Response from Vault configuration: $response"
 
-    sleep 1
+    check_response "$response" "$port"
 
-    echo "Creating consumer group for APISIX instance on port $port"
     response=$(curl -i -X PUT \
         -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' \
         -d '{
@@ -33,11 +44,15 @@ do
         }' \
         "http://localhost:$port/apisix/admin/consumer_groups" 2>&1)
 
+    check_response "$response" "$port"
+
 done
 
 # Create dummy route for each APISIX instance
 # Dummy upstreams are found from /upstream directory
 echo "Creating dummy routes for APISIX instances"
+
+# ROUTE FOR INSTANCE ON PORT 9180(=Admin API port)
 response=$(curl -i -X PUT \
     -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' \
     -d '{
@@ -76,6 +91,8 @@ response=$(curl -i -X PUT \
     }' \
     "http://localhost:9180/apisix/admin/routes" 2>&1)
 
+check_response "$response" "9180"
+
 response=$(curl -i -X PUT \
     -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' \
     -d '{
@@ -113,5 +130,5 @@ response=$(curl -i -X PUT \
         }
     }' \
     "http://localhost:9280/apisix/admin/routes" 2>&1)
-    
-    #echo "Response from route creation: $response"
+
+check_response "$response" "9280"
