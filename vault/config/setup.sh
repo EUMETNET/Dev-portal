0885f3a1a7a7db39fe958f2808fc736b0385a602
aaltonja <jani.aaltonen@cgi.com>
2024-08-29 10:46:47 +0300

    seems not need the additional health check

--- vault/config/setup.sh
+++ vault/config/setup.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
 VAULT_ADDR='http://127.0.0.1:8200'
 
-# Check Vault status
-MAX_ATTEMPTS=5
-ATTEMPT=1
-while [ $ATTEMPT -le $MAX_ATTEMPTS ]; do
-  vault status > /dev/null 2>&1
-  if [ $? -eq 0 ]; then
-    break
-  fi
-  echo "Vault is not ready. Retrying again after 0.5 seconds..."
-  ATTEMPT=$((ATTEMPT + 1))
-  sleep 0.5
-done
-
 if ! vault secrets list | grep -q "${VAULT_SECRET_ENGINE}/"; then
-  vault secrets enable -path="${VAULT_SECRET_ENGINE}" -version=1 kv
+  vault secrets enable -path="${VAULT_SECRET_ENGINE}" -version=1 kv || exit 1
 fi
 
 tee apisix-policy.hcl << EOF
