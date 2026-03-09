63fa1a4ba9a79376bf01561cdadc23c2674da70c
Kristian Harju <kristian.harju@fmi.fi>
2026-03-09 14:31:50 +0200

    Imports

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
