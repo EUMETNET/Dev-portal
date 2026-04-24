0f02b2d46fec4b43ef9060fc58e44a82206c92ce
Kristian Harju <kristian.harju@fmi.fi>
2026-04-24 14:17:35 +0300

    Revert fixes

--- manage-services.sh
+++ manage-services.sh
@@ -88,25 +88,13 @@ _remove() {
 
 # Start external services
 _start_external_services() {
-    docker compose --profile $ENV -p $ENV up -d --build --wait
+    docker compose --profile $ENV -p $ENV up -d --build
 }
 
 # Configure Vault
 _configure_vault() {
-    echo "Waiting for Vault to be ready before configuring it..."
-    for instance in vault-${ENV} vault-2-${ENV}; do
-        counter=0
-        while ! docker exec -e VAULT_ADDR=http://localhost:8200 "$instance" vault status > /dev/null 2>&1; do
-            sleep 2
-            counter=$((counter+1))
-            if [ $counter -ge 15 ]; then
-                echo "Vault ($instance) did not become healthy after 15 attempts. Exiting..."
-                remove $ENV; exit 1
-            fi
-            echo "Vault ($instance) not ready yet. Waiting 2 seconds before next check..."
-        done
-        docker exec -e VAULT_SECRET_ENGINE=${VAULT_SECRET_ENGINE} -i "$instance" sh /vault/config/setup.sh || { remove $ENV; exit 1; }
-    done
+    docker exec -e VAULT_SECRET_ENGINE=${VAULT_SECRET_ENGINE} -i vault-${ENV} sh /vault/config/setup.sh || { remove $ENV; exit 1; }
+    docker exec -e VAULT_SECRET_ENGINE=${VAULT_SECRET_ENGINE} -i vault-2-${ENV} sh /vault/config/setup.sh || { remove $ENV; exit 1; }
 }
 
 # Configure Keycloak
